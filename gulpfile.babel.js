fcae08119bef0831057616bc89aa5006438b5c05
LeafSan <cwk0906@naver.com>
2022-10-07 22:22:08 +0900

    pug, scss, js done

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,82 @@
 import gulp from "gulp";
+import gpug from "gulp-pug";
+import del from "del";
+import webserver from "gulp-webserver";
+import gimage from "gulp-image";
+import gulp_sass from "gulp-sass";
+import node_sass from "node-sass";
+import autoPrefixer from "gulp-autoprefixer";
+import miniCSS from "gulp-csso";
+import brom from "gulp-bro";
+import babelify from "babelify";
+
+const sass = gulp_sass(node_sass);
+
+const routes = {
+    pug: {
+        watch: "src/**/*.pug",
+        src: "src/*.pug",
+        dest: "build",
+    },
+    img: {
+        src: "src/img/*",
+        dest: "build/img",
+    },
+    scss: {
+        watch: "src/scss/**/*.scss",
+        src: "src/scss/style.scss",
+        dest: "build/css",
+    },
+    js: {
+        watch: "src/js/**/*.js",
+        src: "src/js/main.js",
+        dest: "build/js",
+    },
+};
+
+const clean = () => del(["build"]);
+
+const pug = () =>
+    gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+
+const ws = () =>
+    gulp.src("build").pipe(webserver({ livereload: true, open: true }));
+
+const img = () =>
+    gulp.src(routes.img.src).pipe(gimage()).pipe(gulp.dest(routes.img.dest));
+
+const styles = () =>
+    gulp
+        .src(routes.scss.src)
+        .pipe(sass().on("error", sass.logError))
+        .pipe(autoPrefixer({ browers: ["last 2 versions"] }))
+        .pipe(miniCSS())
+        .pipe(gulp.dest(routes.scss.dest));
+
+const js = () =>
+    gulp
+        .src(routes.js.src)
+        .pipe(
+            brom({
+                transform: [
+                    babelify.configure({ presets: ["@babel/preset-env"] }),
+                    ["uglifyify", { global: true }],
+                ],
+            })
+        )
+        .pipe(gulp.dest(routes.js.dest));
+
+const watch = () => {
+    gulp.watch(routes.pug.watch, pug);
+    gulp.watch(routes.img.src, img);
+    gulp.watch(routes.scss.watch, styles);
+    gulp.watch(routes.js.watch, js);
+};
+
+const prepare = gulp.series([clean, img]);
+
+const assets = gulp.series([pug, styles, js]);
+
+const postDev = gulp.parallel([ws, watch]);
+
+export const dev = gulp.series([prepare, assets, postDev]);
